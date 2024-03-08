73dbd303820466f2ab53555686d75b3cdeffe968
Anne Vos <annevos2006@gmail.com>
2024-03-08 13:38:33 +0100

    logos added

--- juke/delete.php
+++ juke/delete.php
@@ -29,6 +29,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/icon.png">
     <style>
         .words {
             font-family: "Inter", sans-serif;
